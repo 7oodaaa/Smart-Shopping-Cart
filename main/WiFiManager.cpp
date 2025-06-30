0aef4d3f839ae4a6b86ae84c2515cae46f24d299
7oodaaa <mahmoudyasser7422@gmail.com>
2025-06-30 19:15:51 +0300

    Initial commit: Smart Shopping Cart project

---
+++ main/WiFiManager.cpp
@@ -0,0 +1,35 @@
+// WiFiManager.cpp
+#include "WiFiManager.h"
+
+void connectToWiFi(const char* ssid, const char* password) {
+  if (WiFi.status() == WL_CONNECTED) {
+    return;  // Already connected
+  }
+
+  debugPrint("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  unsigned long startTime = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000) {
+    delay(500);
+    debugPrint(".");
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    debugPrintln("\nConnected to WiFi");
+    debugPrintln("IP Address: " + WiFi.localIP().toString());
+  } else {
+    debugPrintln("\nFailed to connect to WiFi");
+  }
+}
+void checkWiFiReconnect(const char* ssid, const char* password) {
+  // Check WiFi connection periodically
+  static unsigned long lastWiFiCheck = 0;
+  if (millis() - lastWiFiCheck >= 10000) {  // Check every 10 seconds
+    lastWiFiCheck = millis();
+    if (WiFi.status() != WL_CONNECTED) {
+      debugPrintln("WiFi disconnected. Reconnecting...");
+      connectToWiFi(ssid, password);
+    }
+  }
+}
---
+++ main/WiFiManager.h
@@ -0,0 +1,22 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+#include <WiFi.h>
+#include "debug.h"
+
+// Function declarations
+/**
+ * @brief Connects to a WiFi network using the provided credentials.
+ * @param ssid The WiFi network SSID.
+ * @param password The WiFi network password.
+ * @return void
+ */
+void connectToWiFi(const char* ssid, const char* password);
+/**
+ * @brief Periodically checks and reconnects to WiFi if disconnected.
+ * @param ssid The WiFi network SSID.
+ * @param password The WiFi network password.
+ * @return void
+ */
+void checkWiFiReconnect(const char* ssid, const char* password);
+
+#endif
